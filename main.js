ca9d7d3d6cb3253522848d7718083163bb48fda3
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-04-02 19:46:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ window.OneSignal = window.OneSignal || [];
                method: 'POST',               
                 headers: {               
                   "Content-type": "application/json; charset=utf-8",
-                  "Access-Control-Allow-Origin": "https://onesignal.com/api/v1/notifications/eb82fca8-dbb2-43db-a60e-bf7a8a550b68?app_id=0727e2e0-25b1-456a-9e64-034a935c0878",
+                  "Access-Control-Allow-Origin": "https://onesignal.com",
                   "Access-Control-Allow-Headers": "Accept",                  
                   "Authorization": "Basic NzJjNTg0NzUtMzU2Zi00OTExLTgzMTktZmJjM2Y5NDQ5Y2E4"
                 }
