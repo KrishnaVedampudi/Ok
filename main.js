973f7348f183cdc64b4bbea80f87ba4f45fa159c
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-04-02 20:09:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ window.OneSignal = window.OneSignal || [];
 
        fetch('https://onesignal.com/api/v1/notifications/eb82fca8-dbb2-43db-a60e-bf7a8a550b68?app_id=0727e2e0-25b1-456a-9e64-034a935c0878', {
                method: 'POST',   
-               mode:'no-cors'
+               mode:'no-cors',
                 headers: {               
                   "Content-type": "application/json; charset=utf-8",                   
                   "Authorization": "Basic NzJjNTg0NzUtMzU2Zi00OTExLTgzMTktZmJjM2Y5NDQ5Y2E4"
