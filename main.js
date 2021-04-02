f15451602a8ca4f36ff2e2285cabc9c9ea4cdad6
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-04-02 19:18:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ window.OneSignal = window.OneSignal || [];
       },
        subdomainName: "system-mk",
     });
-  });                                                                                              
+  });    
+
        fetch('https://onesignal.com/api/v1/notifications/eb82fca8-dbb2-43db-a60e-bf7a8a550b68?app_id=0727e2e0-25b1-456a-9e64-034a935c0878', {
-               method: 'POST',
-               mode: 'no-cors',
+               method: 'POST',              
                 headers: {               
                   "Content-type": "application/json; charset=utf-8",
                   "Authorization": "Basic NzJjNTg0NzUtMzU2Zi00OTExLTgzMTktZmJjM2Y5NDQ5Y2E4"
