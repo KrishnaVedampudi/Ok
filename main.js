0d087ceb3679401b7b4f08e359e1685d4ccc0995
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-04-02 19:24:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,10 +10,11 @@ window.OneSignal = window.OneSignal || [];
   });    
 
        fetch('https://onesignal.com/api/v1/notifications/eb82fca8-dbb2-43db-a60e-bf7a8a550b68?app_id=0727e2e0-25b1-456a-9e64-034a935c0878', {
-               method: 'POST',              
+               method: 'POST',               
                 headers: {               
                   "Content-type": "application/json; charset=utf-8",
                   "Access-Control-Allow-Origin": "https://onesignal.com/api/v1/notifications/eb82fca8-dbb2-43db-a60e-bf7a8a550b68?app_id=0727e2e0-25b1-456a-9e64-034a935c0878",
+                  "Access-Control-Allow-Headers": "Accept"
                   "Authorization": "Basic NzJjNTg0NzUtMzU2Zi00OTExLTgzMTktZmJjM2Y5NDQ5Y2E4"
                 }
            }).then(response => response.json())
